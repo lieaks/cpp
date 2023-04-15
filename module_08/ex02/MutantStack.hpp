c60200759f76d9b98e466d169621608a96267d2f
davidlejaune <david_ly@hotmail.fr>
2023-04-16 01:34:58 +0200

    ex02 done

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -1,12 +1,25 @@
 #pragma once
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
 
-class MutantStack
+#include <stack>
+#include <iostream>
+
+template <typename T>
+class MutantStack : public std::stack<T>
 {
 public:
-	MutantStack();
-	MutantStack(const MutantStack &src);
-	~MutantStack();
-	MutantStack & operator = (const MutantStack &rhs);
+	MutantStack<T>() {};
+	MutantStack<T>(const MutantStack<T> &) {};
+	virtual ~MutantStack<T>() {};
+	MutantStack<T> & operator = (const MutantStack<T> &) {};
 
-private:
+	typedef typename std::stack<T>::container_type::iterator		iterator;
+	typedef typename std::stack<T>::container_type::const_iterator	const_iterator;
+	iterator begin() {return this->c.begin();};
+	const_iterator begin() const {return this->c.begin();};
+	iterator end() {return this->c.end();};
+	const_iterator end() const {return this->c.end();};
 };
+
+#endif //MUTANTSTACK_HPP
